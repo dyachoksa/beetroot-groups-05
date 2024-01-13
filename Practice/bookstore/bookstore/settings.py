2accfd2d6b3c856e5417bcb2ddb3215c104ab7bf
Serhii Diachok <sergey@sdyachok.com.ua>
2024-01-13 12:17:41 +0200

    Django practice: templates and static files

--- Practice/bookstore/bookstore/settings.py
+++ Practice/bookstore/bookstore/settings.py
@@ -53,7 +53,9 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "bookstore" / "templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "bookstore" / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
